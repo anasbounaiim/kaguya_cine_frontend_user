891a23c5d7354c02f278aefc6eadd0a18b08d35f
Anas Bounaim <a_bounaim@etu.enset-media.ac.ma>
2025-06-26 13:19:59 +0100

    Refactor Next.js configuration to use CommonJS syntax and remove TypeScript type annotations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     domains: ['image.tmdb.org'],
   },
@@ -9,4 +9,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
